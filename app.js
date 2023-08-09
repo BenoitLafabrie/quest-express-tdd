2f94a2965ea2dd7a84b2f99e03cbae27e98b3284
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-08-09 11:32:03 +0200

    Challenge in progress

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
-// app.js
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+const connection = require("./connection");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// GET
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
+app.get("/bookmarks/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  connection
+    .query("select * from bookmark where id = ?", [id])
+    .then(([bookmarks]) => {
+      if (bookmarks[0] != null) {
+        res.json(bookmarks[0]);
+      } else {
+        res.status(404).send("Bookmark not found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+});
+
+// POST
+app.post("/bookmarks", (req, res) => {
+  const { url, title } = req.body;
+  if (!url || !title) {
+    return res.status(422).json({ error: "required field(s) missing" });
+  }
+  connection.query("INSERT INTO bookmark SET ?", req.body, (err, stats) => {
+    if (err) return res.status(500).json({ error: err.message, sql: err.sql });
+
+    connection.query(
+      "SELECT * FROM bookmark WHERE id = ?",
+      stats.insertId,
+      (err, records) => {
+        if (err)
+          return res.status(500).json({ error: err.message, sql: err.sql });
+        return res.status(201).json(records[0]);
+      },
+    );
+  });
+});
 
 module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,7 +1,17 @@
 const request = require("supertest");
 const app = require("../app");
+const connection = require("../connection");
+const { response } = require("express");
 
 describe("Test routes", () => {
+  // Clearing table before tests
+
+  beforeEach((done) => {
+    connection.query("TRUNCATE bookmark", done);
+  });
+
+  // GET tests
+
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get("/")
@@ -9,6 +19,62 @@ describe("Test routes", () => {
       .expect("Content-Type", /json/)
       .then((response) => {
         const expected = { message: "Hello World!" };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+  describe("GET /bookmarks/:id", () => {
+    const testBookmark = { url: "https://nodejs.org/", title: "Node.js" };
+    beforeEach((done) => {
+      connection.query("TRUNCATE bookmark", () => {
+        connection.query("INSERT INTO bookmark SET ?", testBookmark, done);
+      });
+    });
+    it("GET /bookmarks/:id failed", (done) => {
+      request(app)
+        .get(`/bookmarks/${response.id}`)
+        .expect(404)
+        .then((response) => {
+          const expected = { error: "Bookmark not found" };
+          expect(response.body).toEqual(expected);
+          done();
+        });
+    });
+    it("GET /bookmarks/:id success", (done) => {
+      request(app)
+        .get(`/bookmarks/1`)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(testBookmark);
+          done();
+        });
+    });
+  });
+
+  // POST tests
+
+  it("POST /bookmarks failed", (done) => {
+    request(app)
+      .post("/bookmarks")
+      .send({})
+      .expect(422)
+      .then((response) => {
+        const expected = { error: "required field(s) missing" };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+  it("POST /bookmarks success", (done) => {
+    request(app)
+      .post("/bookmarks")
+      .send({ url: "https://jestjs.io", title: "Jest" })
+      .expect(201)
+      .then((response) => {
+        const expected = {
+          id: expect.any(Number),
+          url: "https://jestjs.io",
+          title: "Jest",
+        };
         expect(response.body).toEqual(expected);
         done();
       });
